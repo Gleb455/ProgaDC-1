cf8c448853412a2163418cc89151f3454201055d
ElderEv1l <kiryanov.gleb39@gmail.com>
2021-11-26 21:48:59 +0300

    Download

---
+++ Proga2.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double a;
+
+    printf("Enter alpha: ");
+    scanf("%lf", &a);
+
+    printf("z1 = %lf\n", (sin(2 * a) + sin(5 * a) - sin(3 * a)) / (cos(a) + 1 - 2 * sin(2 * a) * sin(2 * a)));
+    printf("z2 = %lf\n", 2 * sin(a));
+
+    return 0;
+}
