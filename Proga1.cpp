10c1709690cd0c9f8e1e1cbd4e0a845696a50e71
ElderEv1l <kiryanov.gleb39@gmail.com>
2021-11-26 21:32:37 +0300

    Download

---
+++ Proga1.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[15];
+    int n;
+
+    printf("Enter the string: ");
+    scanf("%s", s);
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    printf("Your string: %s\n", s);
+    printf("Your integer: %d\n", n);
+
+    return 0;
+}
